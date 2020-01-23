f22b099daccc6c3e50f937ce51348c7ba8bef079
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-23 16:56:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
 def unsafe?(speed)
+  
+speed <60
+return TRUE
+
+speed >40 
+return TRUE
+
+speed >60 == speed <40
+return FALSE
+
+
 
 end
 
 
 
-def not_safe?(speed)
+#def not_safe?(speed)
 	
-end
+#end
 	
 
 
